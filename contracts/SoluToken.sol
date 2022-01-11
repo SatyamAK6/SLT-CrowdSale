66c6c90a13afc118b73628bc5c254a6c8965ead5
SatyamAK6 <56645225+SatyamAK6@users.noreply.github.com>
2022-01-11 10:22:15 +0530

    set fix supply

--- contracts/SoluToken.sol
+++ contracts/SoluToken.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract SoluToken is ERC20, ERC20Detailed  {
-    constructor(uint256 _initialSupply) ERC20Detailed('SoluToken', 'SLT', 18) public {
-        _mint(msg.sender, _initialSupply * 10 ** 18);
+    constructor() ERC20Detailed('SoluToken', 'SLT', 18) public {
+        _mint(msg.sender, 1000000000 * 10 ** 18);
     }
 }
